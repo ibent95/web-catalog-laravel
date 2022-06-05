70e617fde57731e0996a61c5ed63adcc0de22d6e
Ari Satrio <arisatrioputra03@gmail.com>
2022-06-05 18:03:45 +0700

    set up docker

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -13,6 +14,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // return ['data' => getenv()];
         $response = [
             'content' => 'home',
         ];
